e28deb170ab7b5e2608589743069960934c9ed70
App Academy Student <student@appacademy.io>
2014-04-08 14:52:24 -0700

    add movement logic for sliding pieces

--- Chess.rb
+++ Chess.rb
@@ -4,17 +4,81 @@ class Piece
     @pos_x = pos_x
     @pos_y = pos_y
     @board = board
+    board[@pos_x][@pos_y] = self
+    @legal_moves = []
   end
-  def moves
 
+  def legal_moves
+    raise NotYetImplemented
   end
+
+
+  def move(new_pos)
+    if @legal_moves.include?(new_pos)
+      @board[@pos_x][@pos_y] = nil
+      @pos_x, @pos_y = new_pos[0], new_pos[1]
+      @board[@pos_x][@pos_y] = self
+    end
+  end
+
   def move_legal?()
     raise NotYetImplemented
   end
 end
 
-class SlidingPiece
+class SlidingPiece < Piece
 
+  def initialize(color, pos_x, pos_y, board)
+    super(color, pos_x, pos_y, board)
+    @DELTAS = nil
+  end
+
+  def legal_moves
+    @DELTAS.each { |delta| check_lines(delta) }
+  end
+
+  def check_line(delta)
+    candidate_x = @pos_x+delta[0]
+    candidate_y = @pos_y+delta[1]
+
+    #returns possible moves in a line
+    while legal_move?([candidate_x, candidate_y])
+      @legal_moves << [candidate_x, candidate_y]
+      break if !@board[[candidate_x,candidate_y]].nil? && (@board[[candidate_x,candidate_y]].color != @color)
+      candidate_x, candidate_y = candidate_x + delta[0], candidate_y + delta[1]
+    end
+
+    @legal_moves
+  end
+
+  def legal_move?(pos)
+    return false if pos[0] < 0 || pos[0] > 7 ||
+       pos[1] < 0 || pos[1] > 7
+
+    return false if !@board[pos].nil? && @color == @board[pos].color
+    true
+  end
+end
+
+class Bishop < SlidingPiece
+
+  def initialize(color, pos_x, pos_y, board)
+    super(color, pos_x, pos_y, board)
+    @DELTAS =[[1,1],[-1,-1],[1,-1],[-1,1]]
+    @display = "B"
+  end
+
+  def inspect
+    @display
+  end
+
+end
+
+class SteppingPiece
+
+end
+
+class Pawn
 end
 
 
@@ -35,6 +99,23 @@ class Board < Array
 
   def remove_piece()
 
+  end
+
+  def [](pos)
+    if pos.is_a?(Fixnum)
+      super(pos)
+    else
+      self[pos[0]][pos[1]]
+    end
+  end
+
+  def []=(pos, value)
+    if pos.is_a?(Fixnum)
+      super(pos, value)
+    else
+      x,y = pos[0], pos[1]
+      self[x][y] = value
+    end
   end
 
 end
--- employees.rb
+++ employees.rb
@@ -1,12 +1,41 @@
 class Employee
-
-  def initialize(name, title, salary, boss)
+  def initialize(name, title, salary)
     @name = name
     @title = title
     @salary = salary
-    @boss = boss
+    @employees = []
+  end
+
+  def bonus(multiplier)
+    @salary * multiplier
   end
 end
 
 class Manager < Employee
+  def initialize(name, title, salary, employees = [])
+    super(name, title, salary)
+    @employees = employees
+  end
+
+  def bonus(multiplier)
+    # bonus = @employees.inject { |total, employee| total += employee.salary } * multiplier
+
+    return @salary if @employees.empty?
+
+    underling_bonus = 0
+    @employees.each do |employee|
+      underling_bonus += employee.bonus(1)
+    end
+
+    (@salary + underling_bonus) * multiplier
+  end
 end
+
+
+steve = Employee.new("steve", "underling", 25_000)
+marla = Employee.new("marla", "underling", 30_000)
+bob = Manager.new("bob", "ling", 45_000, [steve, marla])
+bill = Manager.new("bill", "overling", 120_000, [bob])
+
+p bill.bonus(0.10)
+
